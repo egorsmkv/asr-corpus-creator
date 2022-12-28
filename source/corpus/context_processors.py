08f6c2ae0692d75bf102982c67091944eb74b0ac
Yehor Smoliakov <yehors+gh@ukr.net>
2022-12-28 14:16:15 +0200

    Fix host variable

--- source/corpus/context_processors.py
+++ source/corpus/context_processors.py
@@ -1,4 +1,10 @@
 def inject_hostname(request):
+    host = request.get_host()
+
+    if ':' in host:
+        parts = host.split(':')
+        host = parts[0]
+
     return {
-        'HOSTNAME': request.get_host(),
+        'HOSTNAME': host,
     }
