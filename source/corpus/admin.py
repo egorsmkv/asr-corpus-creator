47624d96e7fc0b7d4ff1a824f7f5d91e0f5a01ba
Yehor Smoliakov <yehors@ukr.net>
2023-01-10 21:03:31 +0200

    Enhance admin view

--- source/corpus/admin.py
+++ source/corpus/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import *
 
+
 class UtteranceAdmin(admin.ModelAdmin):
     list_display = ['id', 'collection_key', 'label', 'length', 'snr']
     list_filter = ['collection_key']
@@ -11,11 +12,15 @@ class YoutubeChannelLinkAdmin(admin.ModelAdmin):
     list_filter = ['is_exported']
 
 
+class AudioChunkAdmin(admin.ModelAdmin):
+    list_display = ['id', 'filename', 'length']
+
+
 admin.site.register(YoutubeLink)
 admin.site.register(YoutubeChannelLink, YoutubeChannelLinkAdmin)
 admin.site.register(AudioFile)
 admin.site.register(VideoFile)
-admin.site.register(AudioChunk)
+admin.site.register(AudioChunk, AudioChunkAdmin)
 admin.site.register(Utterance, UtteranceAdmin)
 admin.site.register(AudioLink)
 admin.site.register(SearchHistory)
